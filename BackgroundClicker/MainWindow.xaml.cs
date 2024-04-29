64aad80140bfe95ebbbd65dcebba372fe8b58558
yaggod <yaggod@yandex.ru>
2024-04-29 16:40:05 +0300

    Created main window

--- BackgroundClicker/MainWindow.xaml.cs
+++ BackgroundClicker/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BackgroundClicker
 {
@@ -16,8 +8,24 @@ namespace BackgroundClicker
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		public Clicker LeftClicker
+		{
+			get;
+			set;
+		} = new(true);
+
+		public Clicker RightClicker
+		{
+			get;
+			set;
+		} = new(false);
+
+
 		public MainWindow()
 		{
+			IntPtr hWnd = Process.GetProcessById(5952).MainWindowHandle;
+			LeftClicker.HWnd = hWnd;
+
 			InitializeComponent();
 		}
 	}
