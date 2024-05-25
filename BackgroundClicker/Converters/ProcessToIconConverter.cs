b34a1d99a990a8cd4acbc58c50e90d8a868b0ac6
yaggod <yaggod@yandex.ru>
2024-05-25 14:47:21 +0300

    Add icon in process picker

---
+++ BackgroundClicker/Converters/ProcessToIconConverter.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BackgroundClicker.Converters
+{
+	class ProcessToIconConverter : IValueConverter
+	{
+		public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			try
+			{
+				var process = (value as Process);
+				Icon? icon = Icon.ExtractAssociatedIcon(process.MainModule.FileName);
+				ImageSource imageSource = Imaging.CreateBitmapSourceFromHIcon(
+				icon.Handle,
+				Int32Rect.Empty,
+				BitmapSizeOptions.FromEmptyOptions());
+
+				return imageSource;
+			}
+			catch(Exception)
+			{
+				return null;
+			}
+
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
