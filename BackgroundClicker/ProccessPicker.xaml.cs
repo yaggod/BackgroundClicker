037ac327a8708a9929a325d6d617c4b3f6ffc5b5
yaggod <yaggod@yandex.ru>
2024-05-25 03:15:21 +0300

    add currentProcess property to processpicker

--- BackgroundClicker/ProccessPicker.xaml.cs
+++ BackgroundClicker/ProccessPicker.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace BackgroundClicker
@@ -7,18 +8,32 @@ namespace BackgroundClicker
 	/// <summary>
 	/// Логика взаимодействия для ProccessPicker.xaml
 	/// </summary>
-	public partial class ProccessPicker : UserControl
+	public partial class ProcessPicker : UserControl
 	{
+		public static readonly DependencyProperty CurrentProcessProperty;
+
+		public Process CurrentProcess
+		{
+			get => (Process)GetValue(CurrentProcessProperty);
+			set => SetValue(CurrentProcessProperty, value);
+		}
+
 		public ReadOnlyCollection<Process> Processes
 		{
 			get;
 			private set;
 		}
 
-		public ProccessPicker()
+		public ProcessPicker()
 		{
 			InitializeComponent();
 			Refresh();
+		}
+
+		static ProcessPicker()
+		{
+			CurrentProcessProperty =
+			DependencyProperty.Register("CurrentProcess", typeof(Process), typeof(ProcessPicker));
 		}
 
 		public void Refresh()
