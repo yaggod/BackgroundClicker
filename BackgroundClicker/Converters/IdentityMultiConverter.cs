4d696097470436d124cb346b0662f9253ef5299a
yaggod <yaggod@yandex.ru>
2024-05-25 19:00:34 +0300

    created identity converter

---
+++ BackgroundClicker/Converters/IdentityMultiConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BackgroundClicker.Converters
+{
+	class IdentityMultiConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			return values[0];
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			return [value, value];
+		}
+	}
+}
